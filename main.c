36250e4375159df1b5ff8576cd6e15d1e6e0d140
Aaron Tulino (Aaronjamt) <13600347+aaronjamt@users.noreply.github.com>
2025-02-27 11:25:12 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "canvas/canvas.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
 	printf("Hello, world!\n");
 
 	if (argc < 2) {
 		printf("Please provide a Canvas API token.\n");
 		return 1;
 	}
-	Canvas canvas = new Canvas();
-	if (!canvas.login(argv[1])) {
+	Canvas canvas;
+	if (!canvas.login("")) {
 		printf("Failed to login. Invalid token?\n");
 		return 1;
 	}
