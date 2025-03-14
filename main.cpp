75b32f69b0b18be41f040a61850bd09a9938d1aa
butterpig2 <157614446+butterpig2@users.noreply.github.com>
2025-03-14 05:27:45 -0700

    implement menu interface for dashboard

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string>
 #include "canvas/canvas.hpp"
 
 #define UP_ARROW 65
@@ -26,7 +27,6 @@ int get_key_press() {
 	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     return ch;
 }
-
 int menu(int num_items, const char *items[]) {
 	int selected_item = 0;
 	while (1) {
@@ -55,25 +55,47 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	Canvas canvas(argv[1], argv[2]);
-	printf("\x1b[30;47mHello, %s! Welcome to the Canvas LMS Interface (CLI).\x1b[K\x1b[0m\n", canvas.get_current_user().name());
+	printf("\x1b[30;47mHello, %s! Welcome to the Canvas LMS Interface (CLI).\x1b[K\x1b[0m\n", canvas.get_current_user()->name());
 	printf("Here are your courses:\n");
-	for (Course course : canvas.get_courses()) {
-		printf("> ID #%ld: %s", course.id(), course.name());
-		printf(" | Grade: %.2f%%\n", course.get_current_grade());
+	std::vector<Course> courses = canvas.get_courses();
+	std::vector<std::string> formatted_courses;
+	std::vector<const char *> course_names;
+
+	for (Course course : courses) {
+		std::string realGradeFuckingForRealThisTime;
+		if (course.get_current_grade().has_value()) {
+			realGradeFuckingForRealThisTime = std::to_string(course.get_current_grade().value()) + "%";
+		} else {
+			realGradeFuckingForRealThisTime = "N/A";
+		}
+		formatted_courses.push_back("> ID #" + std::to_string(course.id()) + ": " + course.name() + " | Grade: " + realGradeFuckingForRealThisTime + "\n");
 	}
 
-	printf("\n");
+	for (const std::string &to_c : formatted_courses) {
+		course_names.push_back(to_c.c_str());
+	}
+
+	int selected_item = menu(course_names.size(), course_names.data());
+	Course selected_course = courses[selected_item];
+
+	printf("\e[1;1H\e[2J");
+
+	if (selected_course.get_current_grade().has_value()) {
+		printf("Selected course: ID #%ld: %s | Grade: %.2f%%\n", selected_course.id(), selected_course.name(), selected_course.get_current_grade().value());
+	} else {
+		printf("Selected course: ID #%ld: %s | Grade: N/A\n", selected_course.id(), selected_course.name());
+	}
 
 	Course course = canvas.get_courses()[0];
-	printf("Quizzes for %s:\n", course.name());
-	for (Quiz quiz : course.quizzes()) {
+	printf("Quizzes for %s:\n", selected_course.name());
+	for (Quiz quiz : selected_course.quizzes()) {
 		printf("> ID #%ld: %s\n", quiz.id(), quiz.name());
 	}
 
 	printf("\n");
 
-	printf("Assignments for %s:\n", course.name());
-	for (Assignment assignment : course.assignments()) {
+	printf("Assignments for %s:\n", selected_course.name());
+	for (Assignment assignment : selected_course.assignments()) {
 		printf("> ID #%ld: %s\n", assignment.id(), assignment.name());
 	}
 
