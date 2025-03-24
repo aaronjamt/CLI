0f683c285f2a04904e4d9dc40fe8803710db48fd
Aaron Tulino (Aaronjamt) <13600347+aaronjamt@users.noreply.github.com>
2025-03-24 15:25:16 -0700

    Fix minor whoopsie

--- canvas/canvas.cpp
+++ canvas/canvas.cpp
@@ -17,7 +17,8 @@ std::vector<Course> Canvas::get_courses() {
 
 std::optional<Course> Canvas::get_course_by_id(unsigned long course_id) {
     std::string endpoint = "/api/v1/courses/" + std::to_string(course_id) + "?include[]=total_scores";
-    std::optional<nlohmann::json> json_response = _request(endpoint.c_str());    if (!json_response) return {};
+    std::optional<nlohmann::json> json_response = _request(endpoint.c_str());
+    if (!json_response) return {};
 
     if (json_response)
         return Course(this, json_response.value());
@@ -27,7 +28,8 @@ std::optional<Course> Canvas::get_course_by_id(unsigned long course_id) {
 
 std::optional<User> Canvas::get_current_user() {
     std::string endpoint = "/api/v1/users/self";
-    std::optional<nlohmann::json> json_response = _request(endpoint.c_str());    if (!json_response) return {};
+    std::optional<nlohmann::json> json_response = _request(endpoint.c_str());
+    if (!json_response) return {};
 
     if (json_response)
         return User(this, json_response.value());
